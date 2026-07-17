8d78fc57f8e88855e3ca61f3dec40bc2bc95bb03
ldfundora <lmfundora2@gmail.com>
2026-07-17 09:01:57 -0400

    fix

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,16 +1,19 @@
-import { createClient, type GenericCtx } from "@convex-dev/better-auth"
-import { convex, crossDomain } from "@convex-dev/better-auth/plugins"
-import { components } from "./_generated/api"
-import { DataModel } from "./_generated/dataModel"
-import { query } from "./_generated/server"
-import { betterAuth, type BetterAuthOptions } from "better-auth/minimal"
-import authConfig from "./auth.config"
+import { createClient, type GenericCtx } from "@convex-dev/better-auth";
+import { convex, crossDomain } from "@convex-dev/better-auth/plugins";
+import { components } from "./_generated/api";
+import { DataModel } from "./_generated/dataModel";
+import { query } from "./_generated/server";
+import { betterAuth, type BetterAuthOptions } from "better-auth/minimal";
+import authConfig from "./auth.config";
 
-const siteUrl = process.env.SITE_URL || process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+const siteUrl =
+  process.env.SITE_URL || process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
 
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
-export const authComponent = createClient<DataModel>(components.betterAuth)
+export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
   return betterAuth({
@@ -27,13 +30,12 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
       // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
     ],
-  })
-}
+  });
+};
 
-// Example function for getting the current user
 export const getCurrentUser = query({
   args: {},
   handler: async (ctx) => {
-    return authComponent.getAuthUser(ctx)
+    return authComponent.getAuthUser(ctx);
   },
-})
+});
